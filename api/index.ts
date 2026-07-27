6d69d2b6573a507eb67c44251215735366d72050
shakhaoyat <shakhaoyathossain.12@gmail.com>
2026-07-28 05:24:03 +0600

    add vercel serverless entry point and config

---
+++ api/index.ts
@@ -0,0 +1,17 @@
+import app from '../src/app';
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+async function connectDB() {
+      if (isConnected) return;
+
+      const uri = process.env.MONGODB_URI as string;
+      await mongoose.connect(uri);
+      isConnected = true;
+}
+
+export default async function handler(req: any, res: any) {
+      await connectDB();
+      return app(req, res);
+}
